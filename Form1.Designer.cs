f6368c0ec944a5e3680b3ec3d6a274939a6fe9bb
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2020-12-19 19:36:30 +0300

    login form v2.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,8 +34,8 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button_WOC1 = new ePOSOne.btnProduct.Button_WOC();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button_WOC1 = new ePOSOne.btnProduct.Button_WOC();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -47,9 +47,10 @@
             this.txt_ID.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txt_ID.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.txt_ID.Location = new System.Drawing.Point(129, 304);
+            this.txt_ID.Location = new System.Drawing.Point(145, 380);
+            this.txt_ID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_ID.Name = "txt_ID";
-            this.txt_ID.Size = new System.Drawing.Size(313, 34);
+            this.txt_ID.Size = new System.Drawing.Size(352, 41);
             this.txt_ID.TabIndex = 2;
             this.txt_ID.TextChanged += new System.EventHandler(this.txt_ID_TextChanged);
             // 
@@ -59,18 +60,20 @@
             this.txt_sifre.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txt_sifre.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.txt_sifre.Location = new System.Drawing.Point(129, 387);
+            this.txt_sifre.Location = new System.Drawing.Point(145, 484);
+            this.txt_sifre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_sifre.Name = "txt_sifre";
-            this.txt_sifre.Size = new System.Drawing.Size(313, 34);
+            this.txt_sifre.Size = new System.Drawing.Size(352, 41);
             this.txt_sifre.TabIndex = 3;
             this.txt_sifre.TextChanged += new System.EventHandler(this.txt_sifre_TextChanged);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(56, 375);
+            this.pictureBox2.Location = new System.Drawing.Point(63, 469);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(52, 46);
+            this.pictureBox2.Size = new System.Drawing.Size(58, 58);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -79,9 +82,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(56, 295);
+            this.pictureBox1.Location = new System.Drawing.Point(63, 369);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(52, 43);
+            this.pictureBox1.Size = new System.Drawing.Size(58, 54);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -89,6 +93,17 @@
             // backgroundWorker1
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(145, 58);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(331, 201);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
             // 
             // button_WOC1
             // 
@@ -99,35 +114,25 @@
             this.button_WOC1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button_WOC1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_WOC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button_WOC1.Location = new System.Drawing.Point(179, 482);
+            this.button_WOC1.Location = new System.Drawing.Point(201, 602);
             this.button_WOC1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_WOC1.Name = "button_WOC1";
             this.button_WOC1.OnHoverBorderColor = System.Drawing.Color.Transparent;
             this.button_WOC1.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button_WOC1.OnHoverTextColor = System.Drawing.Color.White;
-            this.button_WOC1.Size = new System.Drawing.Size(186, 67);
+            this.button_WOC1.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(255)))), ((int)(((byte)(226)))));
+            this.button_WOC1.Size = new System.Drawing.Size(209, 84);
             this.button_WOC1.TabIndex = 8;
             this.button_WOC1.Text = "Giriş";
             this.button_WOC1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(255)))), ((int)(((byte)(226)))));
             this.button_WOC1.UseVisualStyleBackColor = true;
             this.button_WOC1.Click += new System.EventHandler(this.button_WOC1_Click);
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(129, 46);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(294, 161);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(533, 714);
+            this.ClientSize = new System.Drawing.Size(600, 892);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.button_WOC1);
             this.Controls.Add(this.txt_sifre);
@@ -135,6 +140,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
