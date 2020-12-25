e2c28823393f52eb87580a973947cc13dcac138b
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2020-12-26 02:21:44 +0300

    Revert "Revert "FormAddEmployee ve FormListEmployee eklendi.""
    This reverts commit 439d6bb83d4cadcc087b523561b1dc236e1901ce.

---
+++ Forms/FormAddEmployee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketProject
+{
+    public partial class FormAddEmployee : Form
+    {
+        List<Market> marketList;
+        public FormAddEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void button_WOC1_Click(object sender, EventArgs e)
+        {
+            string employeeAdress = txtemployeeAdress.Text;
+            string employeeName = txtemployeeName.Text;
+            double employeeSalary = Convert.ToDouble(txtSalary.Text);
+            Market market = marketList[cmbBoxMarket.SelectedIndex];
+
+                
+            Employee employee = new Employee(employeeName, employeeAdress, employeeSalary, market );
+
+            EmployeeManager.addEmployee(employee);
+        }
+
+
+
+        private void FormAddEmployee_Load(object sender, EventArgs e)
+        {
+            marketList = MarketManager.GetMarkets();
+
+            foreach (Market market in marketList)
+            {
+                cmbBoxMarket.Items.Add(market.marketName);
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtemployeName(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
